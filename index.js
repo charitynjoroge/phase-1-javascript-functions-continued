9263228fe5a0fd0fc9834d09d649b2915ffb6532
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-19 17:18:14 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+} 
+    saturdayFun()
+
+
+function mondayWork(plan = "go to the office"){
+    return `This Monday, I will ${plan}.`
+    
+}
+    mondayWork()
+
+
+function wrapAdjective(charity = '*'){
+    return function (adjective = 'special'){
+    return`You are ${charity}${adjective}${charity}!`;}
+}
+
+
+    wrapAdjective()
+
+
